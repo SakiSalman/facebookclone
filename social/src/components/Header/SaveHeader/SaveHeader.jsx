b72071e51199a4d6f2015b6c32dce4e3971b9067
Saki Salman <codersaki98@gmail.com>
2023-02-27 04:19:47 +0600

    Heder for saving Profile Cover image

---
+++ social/src/components/Header/SaveHeader/SaveHeader.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FaGlobeAsia } from 'react-icons/fa'
+import './SaveHeader.css'
+
+const SaveHeader = ({saveCover}) => {
+  return (
+    <>
+
+        <div className="save-header-wrapper">
+            <div className="save-header-container">
+              <div className="save-heade-left">
+                <FaGlobeAsia></FaGlobeAsia> <span>Your cover photo is public</span>
+
+
+              </div>
+              <div className="save-heade-right">
+                <button>Cancel</button>
+                <button className='cover-save' onClick={saveCover}>Save</button>
+              </div>
+            </div>
+        </div>
+    
+    
+    </>
+  )
+}
+
+export default SaveHeader
